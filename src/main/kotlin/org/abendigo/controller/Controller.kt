43e3f9384ed5f8a22d088476812bca4ec0e215cc
Thomas Nappo <technology@live.com>
2016-07-02 02:25:32 -0400

    Forcibly removed JNativeHook copyright message

--- src/main/kotlin/org/abendigo/controller/Controller.kt
+++ src/main/kotlin/org/abendigo/controller/Controller.kt
@@ -7,12 +7,15 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 fun main(args: Array<String>) {
+	val out = System.out
+	System.setOut(null)
 	with(Logger.getLogger(GlobalScreen::class.java.`package`.name)) {
 		level = Level.OFF
 		useParentHandlers = false
 	}
-
 	GlobalScreen.registerNativeHook()
 	GlobalScreen.addNativeKeyListener(Keyboard)
+	System.setOut(out)
+
 	Overlay.isVisible = true
 }
